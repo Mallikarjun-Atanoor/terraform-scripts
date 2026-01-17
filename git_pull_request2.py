55c19883b50aa50d9ea02bb18292694cc1689edf
Mallikarjun Atanoor <mallikarjunatanoor7@gmail.com>
2026-01-17 20:00:41 +0530

    Restructure repo: move Vpc_Peering to repo root

--- git_pull_request2.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-response = requests.get("https://api.github.com/repos/kubernetes/kubernetes/pulls")
-
-full_details = response.json()
-
-for i in range(len(full_details)):
-    print(full_details[i]["user"]["login"])
-
